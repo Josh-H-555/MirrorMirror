3b9e759867830eb2b6987913c25c27e08573c827
Josh Hutchinson <64669694+Josh-H-555@users.noreply.github.com>
2022-04-11 23:15:05 -0700

    Initial Commit
    Project finished!

--- js/site.js
+++ js/site.js
@@ -1,4 +1,39 @@
-//Call Hello World
-function HelloWorld() {
-    alert("Hello world!");
+//Event Listener Instantiation
+document.getElementById("btnSubmit").addEventListener("click", getValues);
+
+//get values from site
+function getValues() {
+
+    document.getElementById("alert").classList.add("invisible");
+
+
+    //don't forget to set it equal to the element's VALUE
+    let givenString = document.getElementById("userString").value;
+
+    let reversedString = reverseString(givenString);
+
+    displayMirror(reversedString);
+}
+
+
+//generate the reversed string
+function reverseString(originalString) {
+
+    let reversedString = "";
+
+    for (let i = originalString.length - 1; i >= 0; --i) {
+        reversedString += originalString[i];
+    }
+
+    return reversedString;
+}
+
+//display the string
+function displayMirror(reversedString) {
+
+    paragraph = document.getElementById("msg");
+
+    paragraph.innerHTML += `Here is your reversed string: ${reverseString}`;
+
+    document.getElementById("alert").classList.remove("invisible");
 }
